1863bc030d9bfdbcd77ba5981069d082b6246ed6
JoanMonserrat <jmonserratca@gmail.com>
2025-08-10 16:03:23 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,9 +1,12 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
     driver.get("https://www.saucedemo.com/v1/")
     yield driver
     driver.quit()
